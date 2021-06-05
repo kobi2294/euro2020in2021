7fe39b9a1c13ae16c0dfb016551f7be4374e9eb2
Kobi Hari <kobi2294@yahoo.com>
2021-06-05 18:30:18 +0300

    showing list of matches and dployed to firebase

--- euro-app/src/app/app.component.ts
+++ euro-app/src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { Match } from './models/match.model';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'euro-app';
+export class AppComponent implements OnInit {
+  matches$!: Observable<Match[]>;
 
-  import() {
-    
+  constructor(private store: AngularFirestore) { }
+
+  ngOnInit(): void {
+    let matches$ = this.store.collection('matches').valueChanges() as Observable<Match[]>;
+    this.matches$ = matches$.pipe(
+      map(all => [...all].sort((m1, m2) => m1.id - m2.id))
+    );
   }
+
 }
