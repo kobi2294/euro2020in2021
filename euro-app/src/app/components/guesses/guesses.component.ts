c715ee80626aa262b1ac29edd9cde428ba266c94
Kobi Hari <kobi2294@yahoo.com>
2021-06-15 00:32:08 +0300

    guesses component and the 24 flags

--- euro-app/src/app/components/guesses/guesses.component.ts
+++ euro-app/src/app/components/guesses/guesses.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { combineLatest, interval, Observable, timer } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 import { MatchRecord } from 'src/app/models/match-record';
 import { Match } from 'src/app/models/match.model';
 import { DataService } from 'src/app/services/data.service';
@@ -20,9 +20,18 @@ export class GuessesComponent implements OnInit {
 
   ngOnInit(): void {
     let now = Date.now().valueOf();
-    this.records$ = this.data.myMatchRecords$.pipe(
-      map(records => records.filter(record => record.date.valueOf() > now))
+
+    this.records$ = combineLatest([timer(0, 60000), this.data.myMatchRecords$]).pipe(
+      tap(val => console.log('future guesses calc', val)), 
+      map(([_, records]) => records
+                    .filter(record => record.date.valueOf() > now)
+                    .filter(record => record.match.home && record.match.away))
     );
+    
+  }
+
+  trackByMatchId(index: number, record: MatchRecord) {
+    return record.match.id;
   }
 
 }
